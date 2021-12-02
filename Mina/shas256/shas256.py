590c4ec6960d214e9f57c99b3a78e11aa0718f95
guillemfarriols <89401334+guillemfarriols@users.noreply.github.com>
2021-12-02 23:34:40 +0100

    Update shas256.py

--- Mina/shas256/shas256.py
+++ Mina/shas256/shas256.py
@@ -1,3 +1,3 @@
-# En "ABC" pots canviar de paraula en comptes d'ABC pots introduir Hola entre les comes "".
+# En "ABC" és possible canviar de paraula en comptes d'ABC introduir "Hola" entre les comes"".
 from hashlib import sha256
 print(sha256("ABC".encode("ascii")).hexdigest())
